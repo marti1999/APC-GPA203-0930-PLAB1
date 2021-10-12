5df18ff502147aa6b328dabfbd59aed8fd196bd2
Ricardlol <ricardlopezolivares@gmail.com>
2021-10-12 20:12:59 +0200

    visualitation

--- projecte/main.py
+++ projecte/main.py
@@ -2,11 +2,13 @@ from sklearn.datasets import make_regression
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import seaborn as sns
 import scipy.stats
+
 
 pd.set_option("display.max_columns", None)
 
-names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO','B', 'LSTAT', 'MEDV']
+names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX','PTRATIO', 'B', 'LSTAT', 'MEDV']
 
 def load_database(path):
     database = pd.read_csv(path, delim_whitespace=True, names=names)
@@ -24,7 +26,7 @@ if __name__ == '__main__':
     print(database.dtypes)
 
     print("\n\tValues NULL")
-    print(database.isnull().sum())
+    print(database.info())
 
     print("\n\tFirst 5 lines in DDBB")
     print(database.head())
@@ -34,3 +36,34 @@ if __name__ == '__main__':
 
     print("\n\tCorrelation Columns:")
     print(database.corr())
+
+
+    def negatives(val):
+
+        if val < 0:
+            color = 'red'
+        else:
+            color = 'black'
+
+        return f'color: {color}'
+
+
+    def undeline(val):
+        if val > 0.7 and val < 1:
+            background = "yellow"
+        else:
+            background = "None"
+
+        return f'background-color:{background}'
+
+    # Solo se ve en jupyter
+    database.corr().style.applymap(undeline).applymap(negatives)
+
+    """for column in database.columns:
+        sns.distplot(database[column])
+        plt.show()
+
+    plt.figure(figsize=(20, 10))
+    sns.heatmap(database.corr(), annot=True, cmap=plt.cm.CMRmap_r)
+
+    sns.pairplot(pd.DataFrame(database))"""
